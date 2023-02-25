b354ba92f0ca53698cbe36318a27629c9a15c6e6
YoungTchalla18 <jolatunj@gmail.com>
2023-02-25 18:43:59 -0500

    added header

--- AngularCRUD/src/app/material/material.module.ts
+++ AngularCRUD/src/app/material/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
-
+const modules = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [modules],
+  imports: [modules],
 })
-export class MaterialModule { }
+export class MaterialModule {}
