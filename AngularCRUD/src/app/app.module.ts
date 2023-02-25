e4487c3bcfc4a41d2a7289d1360916d82922fb0f
YoungTchalla18 <jolatunj@gmail.com>
2023-02-25 18:14:15 -0500

    added create-registration component

--- AngularCRUD/src/app/app.module.ts
+++ AngularCRUD/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent
   ],
   imports: [
     BrowserModule,
