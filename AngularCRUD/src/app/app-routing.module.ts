a67148c0eeecb1028c0d0f68f94a948d2f7213b0
YoungTchalla18 <jolatunj@gmail.com>
2023-02-25 18:22:38 -0500

    added components necessary components

--- AngularCRUD/src/app/app-routing.module.ts
+++ AngularCRUD/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateRegistrationComponent
+  },
+  {
+    path: 'edit/:id',
+    component: CreateRegistrationComponent
+  },
+  {
+    path: 'list',
+    component: RegistrationListComponent
+  },
+  {
+    path: 'detail/:id',
+    component: UserDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularCRUD/src/app/app.module.ts
+++ AngularCRUD/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateRegistrationComponent
+    CreateRegistrationComponent,
+    RegistrationListComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
